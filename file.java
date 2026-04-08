b15b16f31db725caf6ed884e281ec202e88a2b0e
budathati <budathati.kumar@gmail.com>
2026-04-08 23:18:34 +0530

    file.java

---
+++ file.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        String name = "Prasanna";
+        String project = "Docker";
+
+        System.out.println("Hello " + name);
+        System.out.println("Welcome to " + project);
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("Sum is: " + sum);
+    }
+}
